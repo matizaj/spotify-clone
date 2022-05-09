d69811c311b5486e9b799220853fcb9e045113ec
Mateusz <matzaj101@gmail.com>
2022-05-09 23:58:50 +0200

    playlist ui

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,13 +1,30 @@
-﻿import { GetServerSideProps } from "next";
+﻿import { Box } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
+import GradientLayout from "../../components/gradientLayout";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
+const getBgColor = (id) => {
+  const colors = ["red", "green", "blue", "teal", "gray", "yellow", "purple", "pink"];
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
 
 const Playlist = ({ playlist }) => {
-  return <div>{playlist.name}</div>;
+  const color = getBgColor(playlist.id);
+  return (
+    <GradientLayout
+      color={color}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <div>sddfgghdfhf</div>
+    </GradientLayout>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ query, req }) => {
-  console.log("context", query.id as String);
   const { id } = validateToken(req.cookies["spotify-clone-token"]);
   const [playlist] = await prisma.playlist.findMany({
     where: {
