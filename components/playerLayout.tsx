504bb16bf32154bee5123fac920306738737657b
Mateusz <matzaj101@gmail.com>
2022-05-09 22:01:37 +0200

    profile ui

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -4,11 +4,11 @@ import Sidebar from "./sidebar";
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
-      <Box position="absolute" top="0" width="250px" left="0" border="2px">
+      <Box position="absolute" top="0" width="250px" left="0">
         <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
-        Layout here with Vercel deploy! {children}
+        <Box height="calc(100vh - 100px)">{children}</Box>
       </Box>
       <Box position="absolute" left="0" bottom="0" border="2px">
         player
