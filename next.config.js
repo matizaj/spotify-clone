4879b52179273e57949963534685340d32d1a916
Mateusz <matzaj101@gmail.com>
2022-05-11 10:12:28 +0200

    skip eslint

--- next.config.js
+++ next.config.js
@@ -12,6 +12,11 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
